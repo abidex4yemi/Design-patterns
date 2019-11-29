c22363ba0fb93c90141c205db830d1a13c1127e5
abidex4yemi <abidex4yemi@gmail.com>
2019-11-29 17:32:19 +0100

    Create Logger class

---
+++ singleton/logger.js
@@ -0,0 +1,17 @@
+class Logger {
+  constructor() {
+    this.logs = [];
+  }
+
+  get count() {
+    return this.logs.length;
+  }
+
+  log(message) {
+    const timeStamp = new Date().toISOString();
+    this.logs.push({ message, timeStamp });
+    console.log(`${timeStamp}-${message}`);
+  }
+}
+
+module.exports = Logger;
